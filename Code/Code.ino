12d85cc482567f68ca3e444626be05cbdf5959b3
Gabriel Henrique Vasconcelos da Silva <Gabiel Henrique Vasconcelos da Silva>
2021-08-01 11:07:39 -0300

    Code inclusion of Ultrasonic sensor

--- Code/Code.ino
+++ Code/Code.ino
@@ -2,10 +2,12 @@
                       
 void setup() {
   BeginHC12(9600);
-  
+  BeginUltrasonic(9,8,11,10);
+  Serial.begin(9600);
 }
 void loop() {
-
+  Serial.println(Read(9,8));
+  delay(200);
   if(Receive() == 'A')
     Send('A');
 }
